f24c37b103377e0b670c8914d327d25cf03c9fa5
soul97 <nikossl97@gmail.com>
2021-09-13 18:35:27 +0300

    preprocessing pipeline script added

--- preprocess.py
+++ preprocess.py
@@ -27,73 +27,73 @@ def read_dataset(path="dataset/data/", joints=None, attr=None):
         movements = os.listdir(os.path.join(path,p))
         for m in movements:
             if not re.match(f'{p}_[S|M|L]_(0[1-9]|[1-2][0-9]|30).csv',m): continue
-            df = pd.read_csv(os.path.join(path,p,m), index_col="Time", usecols=cols)
+            df = pd.read_csv(os.path.join(path,p,m), usecols=cols)
             data[m]=df
     return data
-            
 
-def all_in_kfold(data):
-    """Split the movements into 10 partitions of approximately equal size (+-1) according to the "all-in" strategy. The movements of each partition are selected randomly.
-       The partitions have approximately the same number of movements (+-1) for a given (participant, object) pair.
+
+def clean_data(mov_data):
+    """Identify the frames with noisy right wrist y-coordinates' detections and filter them out.
 
     Args:
-        data (dictionary): A dictionary with the movement filename as the key and a pandas dataframe containing the corresponding skeletal data of the movement as the value.
+        mov_data (pd.DataFrame): A DataFrame containing the skeletal data of a single movement of the dataset, as given by OpenPose.
 
     Returns:
-        partitions (list): A list containing 10 lists, one for each partition. The nested lists contain the movement filenames of the partition.
+        mov_data (pd.DataFrame): A DataFrame containing the skeletal data of the given movement with frames that have noisy wrist y-coordinates filtered out.
     """
     
-    part_movements = {f"P{p}":{'S':[], 'M':[], 'L':[]} for p in range(1,9)}
-    for mov in data.keys():
-        part_id = mov[:2]
-        obj_id = mov[3:4]
-        part_movements[part_id][obj_id].append(mov)
-    
-    partitions = [[] for p in range(10)]
-    for pfiles in part_movements.values():
-        for plfiles in pfiles.values():
-            random.shuffle(plfiles)
+    PROB_THRESHOLD = 0.6
+    DIST_THRESHOLD = 10
 
-            part_div = len(plfiles) // 10
-            part_mod = len(plfiles) % 10
-            for i in range(10):
-                partitions[i] = partitions[i] + [filename for filename in plfiles[i*part_div:(i+1)*part_div]]
-            
-            partitions = sorted(partitions, key= lambda x: len(x))
-            for i,f in enumerate(range(10*part_div,10*part_div+part_mod)):
-                partitions[i].append(plfiles[f])
-    return partitions
+    mov_data = mov_data.loc[mov_data["RWrist.prob"] > PROB_THRESHOLD]                       # Remove the frames where the detection probability of the right wrist keypoint is less than or equal to 0.6
     
-    
-    
-def one_out_kfold(data):
-    """Split the movements into 8 partitions of approximately equal size according to the "all-in" strategy. The movements of the i-th partition are selected as the movements of the i-th participant.
+    coords = mov_data["RWrist.y"].to_numpy()                                                # Calculate the minimum distance between the right wrist keypoint of the current and previous frame and the current and next frame. 
+    dist = np.abs(coords[1:] - coords[:-1])
+    prev_dist = np.r_[dist[0], dist]
+    next_dist = np.r_[dist, dist[-1]]
+    neighbour_dist = np.min([prev_dist, next_dist], axis=0)
+  
+    mov_data = mov_data[neighbour_dist < DIST_THRESHOLD]                                    # If the distance for a frame is greater than or equal to 10 pixels, the frame is filtered out.
+
+    return mov_data
+
+
+def cut_movement(cleaned_data, mov_data, tail=0, head=0):
+    """Identify and isolate the data that correspond to the grasping phase of the movement and the data that correspond to the given number of frames before (tail) and after (head) the grasping phase.
 
     Args:
-        data (dictionary): A dictionary with the movement filename as the key and a pandas dataframe containing the corresponding skeletal data of the movement as the value.
+        cleaned_data (pd.DataFrame): A DataFrame containing the skeletal data of a single movement of the dataset with the frames with noisy wrist y-coordinates filtered out.
+        mov_data (pd.DataFrame): A DataFrame containing the skeletal data of the same movement of the dataset, as given by OpenPose.
+        tail (int, optional): An integer that indicates how many conseutive frames before the grasping phase to return. If negative or not given, the value defaults to 0.
+        head (int, optional): An integer that indicates how many conseutive frames after the grasping phase to return. If negative or not given, the value defaults to 0.
 
     Returns:
-        partitions (list): A list containing 8 lists, one for each partition. The nested lists contain the movement filenames of the partition.
+        mov_data [pd.DataFrame]: A DataFrame containing the skeletal data of the grasping phase of the given movement and the skeletal data of the given number of frames before and after the 
+                                 grasping movement.
     """
+    
+    WINDOW = 10
+    START_STD = 2.0
+    STOP_STD = 1.5
 
-    part_movements = {f"P{p}":[] for p in range(1,9)}
-    for mov in data.keys():
-        part_movements[mov[:2]].append(mov)
-    partitions = list(part_movements.values())
-    return partitions
+    coords = cleaned_data["RWrist.y"].to_numpy()
 
-def setup_train_test_split(partitions):
-    """Yield one of the k training-test set splits based on the k input partitions. When this generator is called for the i-th time, the i-th partition is selected as test set and the other partitions
-       are concatenated to form the the training set.
+    wrist_std = []                                                                          # Calculate the standard deviation of the right wrist's y-coordinate for a window of 10 frames.
+    for i in range(len(coords)):
+        mean = np.average(coords[i:i+WINDOW])
+        wrist_std.append(np.sqrt(np.average((coords[i:i+WINDOW]-mean)**2)))
+    
+    wrist_std = np.array(wrist_std)                                         
+    peak = np.argmax(wrist_std)                                                             # Identify the peak of the standard deviation distribution.
+    start_ind = np.where(wrist_std[:peak] > START_STD)[0]                                   # The grasping movement starts the first time the standard deviation becomes over 2.
+    end_ind = np.where(wrist_std[peak:] < STOP_STD)[0]                                      # The grasping movement ends after the peak of the standard deviation, the first time the standard deviation becomes less than 1.5.
 
-    Args:
-        partitions (list): A list of lists. Each of the nested lists represents a partition and contains the movement's filenames.
+    start = np.min(np.where(wrist_std[:peak] > START_STD)) + WINDOW - 1                     # The standard deviation is assigned as a measure of speed of the last frame of the window.
+    end = peak + np.min(np.where(wrist_std[peak:] < STOP_STD)) + WINDOW -1
 
-    Yields:
-        split (tuple): A tuple of 2 lists. The first list contains the filenames of the training set and the second list contains the filenames of the testing set.
-    """
-    for i in range(len(partitions)):
-        train_set = list(np.concatenate([partitions[j] for j in range(len(partitions)) if i!=j]))
-        test_set = partitions[i]
-        split = (train_set, test_set)
-        yield split
+    start_time, end_time = cleaned_data.iloc[[start,end]]["Time"]
+
+    start_ind = max(0, mov_data[mov_data["Time"] == start_time].index[0] - max(0,tail))     # Add the given number of frames before the grasping phase.
+    end_ind = mov_data[mov_data["Time"] == end_time].index[0] + max(0, head)                # Add the given number of frames before the grasping phase.
+
+    return mov_data.iloc[start_ind:end_ind]
---
+++ setup_data.py
@@ -0,0 +1,65 @@
+def all_in_kfold(data):
+    """Split the movements into 10 partitions of approximately equal size (+-1) according to the "all-in" strategy. The movements of each partition are selected randomly.
+       The partitions have approximately the same number of movements (+-1) for a given (participant, object) pair.
+
+    Args:
+        data (dictionary): A dictionary with the movement filename as the key and a pandas dataframe containing the corresponding skeletal data of the movement as the value.
+
+    Returns:
+        partitions (list): A list containing 10 lists, one for each partition. The nested lists contain the movement filenames of the partition.
+    """
+    
+    part_movements = {f"P{p}":{'S':[], 'M':[], 'L':[]} for p in range(1,9)}
+    for mov in data.keys():
+        part_id = mov[:2]
+        obj_id = mov[3:4]
+        part_movements[part_id][obj_id].append(mov)
+    
+    partitions = [[] for p in range(10)]
+    for pfiles in part_movements.values():
+        for plfiles in pfiles.values():
+            random.shuffle(plfiles)
+
+            part_div = len(plfiles) // 10
+            part_mod = len(plfiles) % 10
+            for i in range(10):
+                partitions[i] = partitions[i] + [filename for filename in plfiles[i*part_div:(i+1)*part_div]]
+            
+            partitions = sorted(partitions, key= lambda x: len(x))
+            for i,f in enumerate(range(10*part_div,10*part_div+part_mod)):
+                partitions[i].append(plfiles[f])
+    return partitions
+    
+    
+    
+def one_out_kfold(data):
+    """Split the movements into 8 partitions of approximately equal size according to the "all-in" strategy. The movements of the i-th partition are selected as the movements of the i-th participant.
+
+    Args:
+        data (dictionary): A dictionary with the movement filename as the key and a pandas dataframe containing the corresponding skeletal data of the movement as the value.
+
+    Returns:
+        partitions (list): A list containing 8 lists, one for each partition. The nested lists contain the movement filenames of the partition.
+    """
+
+    part_movements = {f"P{p}":[] for p in range(1,9)}
+    for mov in data.keys():
+        part_movements[mov[:2]].append(mov)
+    partitions = list(part_movements.values())
+    return partitions
+
+def setup_train_test_split(partitions):
+    """Yield one of the k training-test set splits based on the k input partitions. When this generator is called for the i-th time, the i-th partition is selected as test set and the other partitions
+       are concatenated to form the the training set.
+
+    Args:
+        partitions (list): A list of lists. Each of the nested lists represents a partition and contains the movement's filenames.
+
+    Yields:
+        split (tuple): A tuple of 2 lists. The first list contains the filenames of the training set and the second list contains the filenames of the testing set.
+    """
+    for i in range(len(partitions)):
+        train_set = list(np.concatenate([partitions[j] for j in range(len(partitions)) if i!=j]))
+        test_set = partitions[i]
+        split = (train_set, test_set)
+        yield split
