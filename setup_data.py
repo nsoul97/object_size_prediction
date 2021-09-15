3a7760b698624c0a165aaf48932fd9bb29208a1a
soul97 <nikossl97@gmail.com>
2021-09-15 12:22:52 +0300

    change setup_train_test_split args

--- setup_data.py
+++ setup_data.py
@@ -3,11 +3,12 @@ import numpy as np
 
 
 def all_in_kfold(data):
-    """Split the movements into 10 partitions of approximately equal size (+-1) according to the "all-in" strategy. The movements of each partition are selected randomly.
-       The partitions have approximately the same number of movements (+-1) for a given (participant, object) pair.
+    """ Split the movements into 10 partitions of approximately equal size (+-1) according to the "all-in" strategy. The movements of each partition are selected randomly.
+        The partitions have approximately the same number of movements (+-1) for a given (participant, object) pair.
 
     Args:
-        data (dictionary): A dictionary with the movement filename as the key and a pandas dataframe containing the corresponding skeletal data of the movement as the value.
+        data (dictionary): A dictionary with the movement filename as the key and a pd.DataFrame containing the corresponding data of the movement as the value. This dictionary contains all the movements
+                           of the dataset.
 
     Returns:
         partitions (list): A list containing 10 lists, one for each partition. The nested lists contain the movement filenames of the partition.
@@ -37,10 +38,11 @@ def all_in_kfold(data):
     
     
 def one_out_kfold(data):
-    """Split the movements into 8 partitions of approximately equal size according to the "all-in" strategy. The movements of the i-th partition are selected as the movements of the i-th participant.
+    """ Split the movements into 8 partitions of approximately equal size according to the "all-in" strategy. The movements of the i-th partition are selected as the movements of the i-th participant.
 
     Args:
-        data (dictionary): A dictionary with the movement filename as the key and a pandas dataframe containing the corresponding skeletal data of the movement as the value.
+        data (dictionary): A dictionary with the movement filename as the key and a pd.DataFrame containing the corresponding data of the movement as the value. This dictionary contains all the movements
+                           of the dataset.
 
     Returns:
         partitions (list): A list containing 8 lists, one for each partition. The nested lists contain the movement filenames of the partition.
@@ -52,16 +54,25 @@ def one_out_kfold(data):
     partitions = list(part_movements.values())
     return partitions
 
-def setup_train_test_split(partitions):
-    """Yield one of the k training-test set splits based on the k input partitions. When this generator is called for the i-th time, the i-th partition is selected as test set and the other partitions
-       are concatenated to form the the training set.
+def setup_train_test_split(data, strategy):
+    """ Yield one of the k training-test set splits based on the k input partitions. When this generator is called for the i-th time, the i-th partition is selected as test set and the other partitions
+        are concatenated to form the the training set.
 
     Args:
-        partitions (list): A list of lists. Each of the nested lists represents a partition and contains the movement's filenames.
+        data (dictionary): A dictionary with the movement filename as the key and a pd.DataFrame containing the corresponding data of the movement as the value. This dictionary contains all the movements
+                           of the dataset.
+        strategy (str): A string that determines the dataset split strategy to be used. The strategy string acceptable values are 'all-in' and 'one-out'.
 
     Yields:
         split (tuple): A tuple of 2 lists. The first list contains the filenames of the training set and the second list contains the filenames of the testing set.
     """
+    if strategy == 'all-in':
+        partitions = all_in_kfold(data)
+    elif strategy == 'one-out':
+        partitions = one_out_kfold(data)
+    else:
+        assert(0)
+    
     for i in range(len(partitions)):
         train_set = list(np.concatenate([partitions[j] for j in range(len(partitions)) if i!=j]))
         test_set = partitions[i]
